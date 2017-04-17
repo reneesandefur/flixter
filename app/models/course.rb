dee1bc1466b81b22b3e78e3b506fe10e9b6594ba
Renee Sandefur <reneersandefur@gmail.com>
2017-04-17 18:30:03 +0000

    added course validation and css for error message

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,6 @@
 class Course < ApplicationRecord
     belongs_to :user
+    
+    validates :title, :description, presence: true
+    validates :cost, presence: true, numericality: {greater_than_or_equal_to: 0}
 end
