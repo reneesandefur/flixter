ed7261a079dacb2e5333eaf62cbae43d0692665c
Renee Sandefur <reneersandefur@gmail.com>
2017-04-20 03:41:48 +0000

    complteted user authentications

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -1,5 +1,7 @@
 class Instructor::CoursesController < ApplicationController
     before_action :authenticate_user!    
+    before_action :require_authorized_for_current_user, only: [:show]
+    
     def new
         @course = Course.new
     end
@@ -10,15 +12,24 @@ class Instructor::CoursesController < ApplicationController
             redirect_to instructor_course_path(@course)
         else
             render :new, status: :unprocessable_entity
-    
         end
     end
     
     def show
-            @course = Course.find(params[:id])
     end
     
     private
+    
+    def require_authorize_for_current_course
+        if current_course.user != current_user
+            render plain: "Unauthorized user", status: :unauthorized
+        end
+    end
+    
+    helper method :current_course
+    def current_course
+        @current_course ||= Course.find(paramas[:id])
+    end 
     
     def course_params
         params.require(:course).permit(:title, :description, :cost)
